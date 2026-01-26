8f0d2395e50991e925887b4b5e9a3a29a56b3f11
DhirajYadav <dhirajyadav2378@gmail.com>
2026-01-26 18:51:13 +0545

    Add trace mesh

---
+++ Source/InventorySystem/Public/Interaction/Components/Ydv_TraceMeshComponent.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "Ydv_TraceMeshComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSYSTEM_API UYdv_TraceMeshComponent : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+};
