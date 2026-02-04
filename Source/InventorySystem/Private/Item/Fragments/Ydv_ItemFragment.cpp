11da226a97b8c815634e7d6a2679e667d14b2e5a
DhirajYadav <dhirajyadav2378@gmail.com>
2026-02-04 22:50:58 +0545

    Add text and image fragment

--- Source/InventorySystem/Private/Item/Fragments/Ydv_ItemFragment.cpp
+++ Source/InventorySystem/Private/Item/Fragments/Ydv_ItemFragment.cpp
@@ -1,5 +1,7 @@
 #include "Item/Fragments/Ydv_ItemFragment.h"
 #include "Widgets/Composite/Ydv_CompositeBase.h"
+#include "Widgets/Composite/Leaf/Ydv_Leaf_Image.h"
+#include "Widgets/Composite/Leaf/Ydv_Leaf_Text.h"
 
 void FYdv_InventoryItemFragment::Assimilate(UYdv_CompositeBase* Composite) const
 {
@@ -10,4 +12,27 @@ void FYdv_InventoryItemFragment::Assimilate(UYdv_CompositeBase* Composite) const
 bool FYdv_InventoryItemFragment::MatchesWidgetTag(const UYdv_CompositeBase* Composite) const
 {
 	return Composite->GetFragmentTag().MatchesTagExact(GetFragmentTag());
+}
+
+void FInv_TextFragment::Assimilate(UYdv_CompositeBase* Composite) const
+{
+	FYdv_InventoryItemFragment::Assimilate(Composite);
+	if (!MatchesWidgetTag(Composite)) return ;
+	
+	UYdv_Leaf_Text* LeafText = Cast<UYdv_Leaf_Text>(Composite);
+	if (!IsValid(LeafText)) return;
+	LeafText->SetText(FragmentText);
+}
+
+void FInv_ImageFragment::Assimilate(UYdv_CompositeBase* Composite) const
+{
+	FYdv_InventoryItemFragment::Assimilate(Composite);
+	if (!MatchesWidgetTag(Composite)) return ;
+	
+	UYdv_Leaf_Image* Image = Cast<UYdv_Leaf_Image>(Composite);
+	if (!IsValid(Image)) return;
+	
+	Image->SetImage(Icon);
+	Image->SetBoxSize(IconDimensions);
+	Image->SetImageSize(IconDimensions);
 }
--- Source/InventorySystem/Public/Item/Fragments/Ydv_ItemFragment.h
+++ Source/InventorySystem/Public/Item/Fragments/Ydv_ItemFragment.h
@@ -27,7 +27,7 @@ private:
 /*
  * Item fragment specifically for assimilation into a widget.
  */
-class UInv_CompositeBase;
+class UYdv_CompositeBase;
 USTRUCT(BlueprintType)
 struct FYdv_InventoryItemFragment : public FYdv_ItemFragment
 {
@@ -36,4 +36,35 @@ struct FYdv_InventoryItemFragment : public FYdv_ItemFragment
 	virtual void Assimilate(UYdv_CompositeBase* Composite) const;
 protected:
 	bool MatchesWidgetTag(const UYdv_CompositeBase* Composite) const;
+};
+
+USTRUCT(BlueprintType)
+struct FInv_TextFragment : public FYdv_InventoryItemFragment
+{
+	GENERATED_BODY()
+
+	FText GetText() const { return FragmentText; }
+	void SetText(const FText& Text) { FragmentText = Text; }
+	virtual void Assimilate(UYdv_CompositeBase* Composite) const override;
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	FText FragmentText;
+};
+USTRUCT(BlueprintType)
+struct FInv_ImageFragment : public FYdv_InventoryItemFragment
+{
+	GENERATED_BODY()
+
+	UTexture2D* GetIcon() const { return Icon; }
+	virtual void Assimilate(UYdv_CompositeBase* Composite) const override;
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	TObjectPtr<UTexture2D> Icon{nullptr};
+
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	FVector2D IconDimensions{44.f, 44.f};
 };
