c0c6fe91243b0d8dad3b2fa135f3d32828f10d0f
DhirajYadav <dhirajyadav2378@gmail.com>
2026-05-30 22:54:21 +0545

    Add Item Tags

---
+++ Source/InventorySystem/Private/Item/Ydv_ItemTags.cpp
@@ -0,0 +1,21 @@
+#include "Item/Ydv_ItemTags.h"
+
+namespace Items
+{
+	namespace Equipable
+	{
+		UE_DEFINE_GAMEPLAY_TAG(Gun, "Item.Equipable.Gun")
+		UE_DEFINE_GAMEPLAY_TAG(Meele, "Item.Equipable.Melee")
+	}
+	namespace Consumable
+	{
+		UE_DEFINE_GAMEPLAY_TAG(HealthPotion, "Item.Consumable.HealthPotion")
+		UE_DEFINE_GAMEPLAY_TAG(StaminaPotion, "Item.Consumable.StaminaPotion")
+		UE_DEFINE_GAMEPLAY_TAG(ArmorPotion, "Item.Consumable.ArmorPotion")
+	}
+	namespace Interactable
+	{
+		
+	}
+	
+}
