45e99c3d09a4ea0d65fe2c077d63f276a6ae5aed
DhirajYadav <dhirajyadav2378@gmail.com>
2026-05-30 22:58:21 +0545

    Add item tag header

---
+++ Source/InventorySystem/Public/Item/Ydv_ItemTags.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "NativeGameplayTags.h"
+
+
+namespace Items
+{
+	namespace Equipable
+	{	
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Gun)
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Melee)
+	}
+	namespace Consumable
+	{
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(HealthPotion)
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(StaminaPotion)
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(ArmorPotion)
+	}
+	namespace Interactable
+	{
+		
+	}
+}
